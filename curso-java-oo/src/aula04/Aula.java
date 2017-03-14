0ffd42fd16d09514e7f0cb58b55ae4157bfd7f49
Augusto <cesarfex@gmail.com>
2017-03-14 12:16:31 -0300

    Primeira versão

---
+++ curso-java-oo/src/aula04/Aula.java
@@ -0,0 +1,20 @@
+package aula04;
+
+public class Aula {
+
+	int x = 1;
+
+	String y = "Olá";
+
+	String z = "Mundo!";
+
+	public static void main {String[] args} {
+
+		Aula a = new Aula();
+		int x2 = a.x +2;
+
+		System.out.println("Valor de x: " + a.x);
+		System.out.println("Valor de x2: " + x2);
+
+ 	}
+}
