ed64ce8a47c7e105a3d8dc8b12e5a63314c2eee4
Augusto <cesarfex@gmail.com>
2017-03-15 13:07:52 -0300

    finalizado etapa de criação do classe, atributos e método

--- curso-java-oo/src/aula04/Aula.java
+++ curso-java-oo/src/aula04/Aula.java
@@ -2,18 +2,22 @@ package aula04;
 
 public class Aula {
 
+	//variáveis de instância
 	int x = 1;
-
 	String y = "Olá";
-
 	String z = "Mundo!";
+
+	// método principal com retorno "void"
 
 	public static void main(String[] args){
 
+		// instância da classe aula (criando objeto do tipo aula)
 		Aula a = new Aula();
-		int x2 = a.x +2;
+		int x2 = a.x +2; //variável local criada dentro do método
 
-		System.out.println("Valor de x: " + a.x);
+		System.out.println("Valor de x: " + a.x); //usando instância da classe para ter acesso ao valor da variável x
 		System.out.println("Valor de x2: " + x2);
+		System.out.println(a.y + " " + a.z);
+
  	}
 }
