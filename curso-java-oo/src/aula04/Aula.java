ff0fa6b923ec0807ec4fdd93551bc95bda34093a
Augusto <cesarfex@gmail.com>
2017-03-15 13:44:24 -0300

    finalizado criação de métodos de instância com tipos de retornos diferentes

--- curso-java-oo/src/aula04/Aula.java
+++ curso-java-oo/src/aula04/Aula.java
@@ -7,8 +7,25 @@ public class Aula {
 	String y = "Olá";
 	String z = "Mundo!";
 
-	// método principal com retorno "void"
 
+	/*todo método criado deve ser definido um tipo de retorno
+	no exemplo abaixo é criado método com retorno do tipo "void" que é um método de retorno vazio*/
+
+	//método de instância não estático
+	void soma (int a, int b){
+		int soma = a + b;
+		System.out.println("O resultado da soma é " + soma);
+	}
+
+	/*todo método criado deve ser definido um tipo de retorno
+	no exemplo abaixo é criado método com retorno do tipo "return" que é um método de retorno do tipo int*/
+
+	int subtracao (int a, int b) {
+		return a - b;
+
+	}
+
+	// método principal com retorno "void"
 	public static void main(String[] args){
 
 		// instância da classe aula (criando objeto do tipo aula)
@@ -17,7 +34,11 @@ public class Aula {
 
 		System.out.println("Valor de x: " + a.x); //usando instância da classe para ter acesso ao valor da variável x
 		System.out.println("Valor de x2: " + x2);
-		System.out.println(a.y + " " + a.z);
+		System.out.println(a.y + " " + a.z); // resultado da concatenação de string
 
+		int sub = a.subtracao(12, 343);
+		System.out.println("O resultado da subtração: " + sub);
+
+		a.soma (2, 5);
  	}
 }
